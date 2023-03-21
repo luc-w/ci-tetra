fc86b5f4a1f07f21906028fb818466e5539d4fa3
Luc Watrin <luc.watrin@gmx.de>
2023-03-21 11:16:41 +0100

    first working version. lots to do.

---
+++ ci_tetra.R
@@ -0,0 +1,111 @@
+library(shiny)
+library(DT)
+library(tidyverse)
+
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("CI Tetra"),
+
+    # Sidebar
+    sidebarLayout(
+        sidebarPanel(
+          
+            DTOutput("my_datatable"),
+            
+            numericInput("alpha", "Alpha level for 1-alpha confidence:", .05, min = .001, max = 1),
+            verbatimTextOutput("value"),
+  
+            actionButton("go",label = "Run simulation")
+  
+        ),
+
+        # Show plot
+        mainPanel(
+          
+           plotOutput("plot_results")
+        
+           )
+    )
+)
+
+
+
+server <- function(input, output) {
+    
+    # initialize an empty dataframe
+    df <- reactiveValues(data = { 
+                         
+                         data.frame(x = numeric(0),y = numeric(0)) %>% 
+                         add_row(x = rep(0,2),y = rep(0,2))
+    
+      })
+
+    # output the datatable based on the dataframe (and make it editable)
+    output$my_datatable <- renderDT({
+      
+        DT::datatable(df$data, 
+                      colnames = c("0", "1"),
+                      rownames = c("0", "1"),
+                      options = list(dom = "t"),
+                      editable = TRUE
+                      #editable = list(target = "column", disable = list(columns=0))
+                      )
+    
+      })
+    
+    # write any edit to initial dataframe
+    # automatically convert negative values to positive
+    
+    observeEvent(input$my_datatable_cell_edit, {
+        
+                # get values
+                info = input$my_datatable_cell_edit
+                i = as.numeric(info$row)
+                j = as.numeric(info$col)
+                k = as.numeric(info$value)
+                
+                if(k < 0){ 
+                    k <- k * -1
+                }
+        
+        # write values to reactive
+        df$data[i,j] <- k
+        
+    })
+
+    # render plot
+    
+    output$plot_results <- renderPlot({
+      
+        req(input$go) # plot only if input button is non-zero
+        
+        #static_df <- isolate(df$data) 
+        
+        range <- NULL
+        
+        for (i in 100:2000) {
+          
+          cit <- ci.tetra(alpha = input$alpha, df$data[1,1]*i/135, 
+                                               df$data[1,2]*i/135, 
+                                               df$data[2,1]*i/135, 
+                                               df$data[2,2]*i/135)
+          range <- rbind(range, cit)
+          
+        }
+        
+        range %>% 
+        data.frame() %>% 
+        mutate(x = (100:2000)/135) %>% 
+        ggplot(aes(x = x, y = Estimate)) +
+        geom_line() + 
+        geom_ribbon(aes(x, ymin = LL, ymax = UL), alpha = 0.4) +
+        scale_y_continuous(breaks = seq(-1, 1, .05))
+
+    })
+    
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
